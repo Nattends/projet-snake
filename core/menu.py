616c7fa5fc5ca34d083be110a106872b1e4dcb1f
Nathan <nathan.nlid@gmail.com>
2025-03-04 18:40:41 +0100

    UPDATE : Avancement des tests

--- core/menu.py
+++ core/menu.py
@@ -23,26 +23,32 @@ class Menu:
     def show(self):
         """Affiche le menu principal"""
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                elif event.type == pygame.MOUSEBUTTONDOWN:
-                    x, y = pygame.mouse.get_pos()
-                    if self.screen_width // 2 - self.jouer_img.get_width() // 2 <= x <= self.screen_width // 2 + self.jouer_img.get_width() // 2 and 400 <= y <= 400 + self.jouer_img.get_height():
-                        game = SnakeGame(self.background_img)
-                        game.run()
-                        return
-                    elif self.screen_width // 2 - self.score_img.get_width() // 2 <= x <= self.screen_width // 2 + self.score_img.get_width() // 2 and 500 <= y <= 500 + self.score_img.get_height():
-                        scores_page = ScorePage(self.background_img)
-                        scores_page.show()
-                    elif self.screen_width // 2 - self.options_img.get_width() // 2 <= x <= self.screen_width // 2 + self.options_img.get_width() // 2 and 600 <= y <= 600 + self.options_img.get_height():
-                        options_page = OptionsPage(self.background_img)
-                        options_page.show()
-
-            self.screen.blit(self.background_img, (0, 0))
-            self.screen.blit(self.logo_img, (self.screen_width // 2 - self.logo_img.get_width() // 2, 200))
-            self.screen.blit(self.jouer_img, (self.screen_width // 2 - self.jouer_img.get_width() // 2, 400))
-            self.screen.blit(self.score_img, (self.screen_width // 2 - self.score_img.get_width() // 2, 500))
-            self.screen.blit(self.options_img, (self.screen_width // 2 - self.options_img.get_width() // 2, 600))
-            pygame.display.flip()
+            try : 
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    elif event.type == pygame.MOUSEBUTTONDOWN:
+                        x, y = pygame.mouse.get_pos()
+                        if self.screen_width // 2 - self.jouer_img.get_width() // 2 <= x <= self.screen_width // 2 + self.jouer_img.get_width() // 2 and 400 <= y <= 400 + self.jouer_img.get_height():
+                            game = SnakeGame(self.background_img)
+                            game.run()
+                            return
+                        elif self.screen_width // 2 - self.score_img.get_width() // 2 <= x <= self.screen_width // 2 + self.score_img.get_width() // 2 and 500 <= y <= 500 + self.score_img.get_height():
+                            scores_page = ScorePage(self.background_img)
+                            scores_page.show()
+                        elif self.screen_width // 2 - self.options_img.get_width() // 2 <= x <= self.screen_width // 2 + self.options_img.get_width() // 2 and 600 <= y <= 600 + self.options_img.get_height():
+                            options_page = OptionsPage(self.background_img)
+                            options_page.show()
+    
+                self.screen.blit(self.background_img, (0, 0))
+                self.screen.blit(self.logo_img, (self.screen_width // 2 - self.logo_img.get_width() // 2, 200))
+                self.screen.blit(self.jouer_img, (self.screen_width // 2 - self.jouer_img.get_width() // 2, 400))
+                self.screen.blit(self.score_img, (self.screen_width // 2 - self.score_img.get_width() // 2, 500))
+                self.screen.blit(self.options_img, (self.screen_width // 2 - self.options_img.get_width() // 2, 600))
+                try : 
+                    pygame.display.flip()
+                except : 
+                    raise ValueError("Erreur lors de l'affichage du menu")
+            except : 
+                pygame.quit()
